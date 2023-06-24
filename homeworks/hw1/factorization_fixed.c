5b4e2a1491e2d3f0e89474eaf7106c53e1870211
seokhyunpark <122147233+seokhyunpark@users.noreply.github.com>
2023-06-24 15:10:37 +0900

    Create factorization_fixed.c

---
+++ homeworks/hw1/factorization_fixed.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, char* argv[]){
+	int i_a, i_b, i_c;
+	double alpha, beta, d_D;
+
+	printf("aX^2 + bX + c = 0\n");
+
+	printf("a: ");
+	scanf("%d", &i_a);
+
+	printf("b: ");
+	scanf("%d", &i_b);
+
+	printf("c: ");
+	scanf("%d", &i_c);
+
+	d_D = i_b*i_b - 4*i_a*i_c;
+	if(d_D >=  0){
+		alpha = ((-1 * i_b) + sqrt(d_D)) / (double)(2 * i_a);
+		beta = ((-1 * i_b) - sqrt(d_D)) / (double)(2 * i_a);
+		printf("%d(x%+.2f)(x%+.2f) = 0\n",i_a, -1 * alpha, -1 * beta);
+	}
+	else
+		printf("Doesn't make sense\n");
+	return 0;
+}
